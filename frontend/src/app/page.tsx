ab3bfba8fe35e1c98ea4ede9c3f9d41411c7a77e
mossimka <maksimsarsekeev@gmail.com>
2025-06-07 21:36:22 +0500

    font, main colors

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,23 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import Styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>Welcome to FoodSnap AI!</div>
+    <main>
+      <header className={Styles.header}>
+        <img src="/images/logo.png" className={Styles.logo}/>
+        <h1>FoodSnap AI</h1>
+      </header>
+      <section className="info">
+        <div className="info-top">
+          <p>Cooking revolution</p>
+        </div>
+        <div>
+          <p>
+            Upload your photo and get <span>recipe immedeately</span>
+          </p>
+        </div>
+      </section>
+    </main>
   );
 }
